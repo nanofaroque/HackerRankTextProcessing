6fdb57e46d3b2d1ec3d90b12b4f3de616ef6567a
Omar Faroque <omar.faroque@realpage.com>
2016-02-24 19:10:40 -0600

    'tr' command practice add

---
+++ Transform.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+### To transfer a character to a different character, linux use a command called "tr"
+
+# change l to L in "Hello World"
+echo "Hello World"|tr 'l' 'L'
+
+#change multiple character to another multiple character
+echo "Hello World"| tr 'World' 'asdfg'
+
+#Spaces tranformed to hypens
+echo "Hello How are you" |tr " " '_'
+
+#Digits delete
+echo "Hello how are you 1234"| tr -d [0-9]
